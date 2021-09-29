c2a05594c11f5641541f51e7079c560de8790ad2
dragon <dydrb219@naver.com>
2021-09-29 22:26:13 +0900

    test

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 import requests
 from bs4 import BeautifulSoup
 
+from datetime import datetime
+
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
@@ -24,12 +26,29 @@ def saving():
     user_receive = request.form['user_give']
     title_receive = request.form['title_give']
     comment_receive = request.form['comment_give']
-    file_receive = request.form['file_give']
+    
+     # File Area
+    file = request.files["file_give"]
+
+    # File에서 '.'으로 시작하는 것 중에서 맨 마지막 '.'만 들고와라!
+    extension = file.filename.split('.')[-1]
+
+    # 현재시각 Data
+    today = datetime.now()
+
+    # year/month/day/hour/minute/second
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+
+    # File 이름 구분 짓기
+    filename = f'file-{mytime}'
+
+    save_to = f'static/{filename}.{extension}'
+    file.save(save_to)
     
     doc = {
         'user':user_receive,
         'title':title_receive,
-        'file':file_receive,
+        'file' : f'{filename}.{extension}',
         'comment':comment_receive
     }
 
