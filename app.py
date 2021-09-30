594a85e8a06dba07601c9e82a85385fa4d960d15
dragon <dydrb219@naver.com>
2021-09-30 23:48:09 +0900

    second commit

--- app.py
+++ app.py
@@ -10,10 +10,14 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
-## HTML을 주는 부분
+# HTML을 주는 부분
 @app.route('/')
 def home():
-   return render_template('snspet.html')
+    return render_template('main.html')
+
+@app.route('/snspet')
+def snspet():
+    return render_template('snspet.html')
 
 @app.route('/sns', methods=['GET'])
 def listing():
@@ -27,7 +31,7 @@ def saving():
     title_receive = request.form['title_give']
     comment_receive = request.form['comment_give']
     
-     # File Area
+    # File Area
     file = request.files["file_give"]
 
     # File에서 '.'으로 시작하는 것 중에서 맨 마지막 '.'만 들고와라!
@@ -49,12 +53,34 @@ def saving():
         'user':user_receive,
         'title':title_receive,
         'file' : f'{filename}.{extension}',
-        'comment':comment_receive
+        'comment':comment_receive,
+        'like':0,
     }
 
     db.sns.insert_one(doc)
 
     return jsonify({'msg':'작성 완료!'})
 
+@app.route('/api/like', methods=['POST'])
+def like_star():
+    user_receive = request.form['user_give']
+
+    target_star = db.sns.find_one({'user': user_receive})
+    current_like = target_star['like']
+
+    new_like = current_like + 1
+
+    db.sns.update_one({'user': user_receive}, {'$set': {'like': new_like}})
+
+    return jsonify({'msg': '좋아요 완료!'})   
+
+@app.route('/api/delete', methods=['POST'])
+def delete_star():
+    user_receive = request.form['user_give']
+
+    db.sns.delete_one({'user': user_receive})
+
+    return jsonify({'msg': '삭제 완료!'})
+
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0',port=5000,debug=True)
